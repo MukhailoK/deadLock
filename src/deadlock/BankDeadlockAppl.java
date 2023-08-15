64ed9f8c19cfa4d6af37f27d734e3208dfc7970e
MukhailoK <96546140+MukhailoK@users.noreply.github.com>
2023-08-16 00:37:45 +0200

    semaphore

--- src/deadlock/BankDeadlockAppl.java
+++ src/deadlock/BankDeadlockAppl.java
@@ -1,13 +1,16 @@
 package deadlock;
 
+import java.util.concurrent.Semaphore;
+
 public class BankDeadlockAppl {
     public static void main(String[] args) throws InterruptedException {
+        Semaphore semaphore = new Semaphore(1);
         Account father = new Account(1000);
         Account son = new Account(2000);
         father.credit(1000);
         son.credit(1000);
-        Transfer transfer1 = new Transfer(father, son, 900);
-        Transfer transfer2 = new Transfer(son, father, 900);
+        Transfer transfer1 = new Transfer(father, son, 900, semaphore);
+        Transfer transfer2 = new Transfer(son, father, 900, semaphore);
         Thread thread1 = new Thread(transfer1);
         Thread thread2 = new Thread(transfer2);
         thread1.start();
--- src/deadlock/Transfer.java
+++ src/deadlock/Transfer.java
@@ -1,44 +1,47 @@
 package deadlock;
 
+import java.util.concurrent.Semaphore;
+
 public class Transfer implements Runnable {
     Account accDonor;
     Account accRecipient;
     int sum;
+    Semaphore semaphore;
 
-    public Transfer(Account accDonor, Account accRecipient, int sum) {
+    public Transfer(Account accDonor, Account accRecipient, int sum, Semaphore semaphore) {
         this.accDonor = accDonor;
         this.accRecipient = accRecipient;
         this.sum = sum;
+        this.semaphore = semaphore;
     }
 
     @Override
     public void run() {
-        Account firstLock, secondLock;
 
-        if (accDonor.hashCode() < accRecipient.hashCode()) {
-            firstLock = accDonor;
-            secondLock = accRecipient;
-        } else {
-            firstLock = accRecipient;
-            secondLock = accDonor;
-        }
-        synchronized (firstLock) {
-            synchronized (secondLock) {
-                transferMoney(accDonor, accRecipient, sum);
-            }
-        }
+        transferMoney(accDonor, accRecipient, sum);
+
     }
 
     private void transferMoney(Account accFrom, Account accTo, int sum) {
         try {
-            Thread.sleep(100);
+            semaphore.acquire();
+            synchronized (accFrom) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (accTo) {
+                    if (accFrom.getBalance() >= sum) {
+                        accFrom.debit(sum);
+                        accTo.credit(sum);
+                    }
+                }
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
-
-        if (accFrom.getBalance() >= sum) {
-            accFrom.debit(sum);
-            accTo.credit(sum);
+        } finally {
+            semaphore.release();
         }
     }
 }
